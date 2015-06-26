56ccaa11a5acfd572179e33e2214d9245aa88536
Jason <yanjia23@msn.com>
2015-06-26 11:48:18 -0400

    6/26/15 - added vector type jaysort

--- WebDerm/src/Algorithms/jaysort.cpp
+++ WebDerm/src/Algorithms/jaysort.cpp
@@ -206,3 +206,90 @@ void jaysort(deque<deque<double> > &vec, deque<int> &origPos, deque<int> sortKey
 		sortedVec.shrink_to_fit();
 	}
 }
+
+void jaysort(vector<double> &vec, vector<int> &origPos) {
+	vector<double> sortedVec;
+	origPos.clear();
+	double val=0;
+	for(unsigned int i=0; i<vec.size(); i++) {
+		val=vec.at(i);
+		if(sortedVec.size()==0) {
+			sortedVec.push_back(val);
+			origPos.push_back(i);
+		}
+		else {
+			for(unsigned int j=0; j<sortedVec.size(); j++) {
+				if((sortedVec.size()-j)<=1) {
+					if(val>=sortedVec.at(j)) {
+						sortedVec.push_back(val);
+						origPos.push_back(i);
+						break;
+					}
+					else {
+						sortedVec.insert(sortedVec.begin(),val);
+						origPos.insert(origPos.begin(),i);
+						break;
+					}
+				}
+				else {
+					if(val>=sortedVec.at(j) && val<=sortedVec.at(j+1)) {
+						sortedVec.insert(sortedVec.begin()+j+1,val);
+						origPos.insert(origPos.begin()+j+1,i);
+						break;
+					}
+					else if(val<=sortedVec.at(j)) {
+						sortedVec.insert(sortedVec.begin(),val);
+						origPos.insert(origPos.begin(),i);
+						break;
+					}
+				}
+			}
+		}
+	}
+	vec = sortedVec;
+	sortedVec.clear();
+	sortedVec.shrink_to_fit();
+}
+
+void jaysort(vector<int> &vec, vector<int> &origPos) {
+	vector<int> sortedVec;
+	int val=0;
+	for(unsigned int i=0; i<vec.size(); i++) {
+		val=vec.at(i);
+		if(sortedVec.size()==0) {
+			sortedVec.push_back(val);
+			origPos.push_back(i);
+		}
+		else {
+			for(unsigned int j=0; j<sortedVec.size(); j++) {
+				if((sortedVec.size()-j)<=1) {
+					if(val>=sortedVec.at(j)) {
+						sortedVec.push_back(val);
+						origPos.push_back(i);
+						break;
+					}
+					else {
+						sortedVec.insert(sortedVec.begin(),val);
+						origPos.insert(origPos.begin(),i);
+						break;
+					}
+				}
+				else {
+					if(val>=sortedVec.at(j) && val<sortedVec.at(j+1)) {
+						sortedVec.insert(sortedVec.begin()+j+1,val);
+						origPos.insert(origPos.begin()+j+1,i);
+						break;
+					}
+					if(val<=sortedVec.at(j)) {
+						sortedVec.insert(sortedVec.begin(),val);
+						origPos.insert(origPos.begin(),i);
+						break;
+					}
+				}
+			}
+		}
+	}
+	vec = sortedVec;
+	sortedVec.clear();
+	sortedVec.shrink_to_fit();
+}
