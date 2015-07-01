2457806fceed77ebadcb102e2f1decd38b5f2aeb
Jason <yanjia23@msn.com>
2015-07-01 19:04:40 -0400

    7/1/15 - implemented ShadeMatch::shade_translation(), not finished

--- WebDerm/src/ShadeShape/ShadeMatch/shadematch.cpp
+++ WebDerm/src/ShadeShape/ShadeMatch/shadematch.cpp
@@ -6,4 +6,49 @@
  */
 
 #include "shadematch.h"
+#include "../shadeshape.h"
+#include "../islands.h"
+#include "/home/jason/git/WebDerm/WebDerm/src/Algorithms/jaysort.h"
 
+/*************** PRIVATE FUNCTIONS *******************/
+
+/*************** PUBLIC FUNCTIONS ********************/
+
+bool ShadeMatch::shade_translation(vector<vector<vector<Islands> > > &islandVec, float totalArea) {
+	float thresh = totalArea / 3.0;
+	vector<vector<vector<Islands> > > newIslandVec = islandVec;
+	vector<vector<int> > indexVec2d;
+	vector<int> areaVec;
+	vector<int> indexVec;
+	for(unsigned int i=0; i<islandVec.size(); i++) {
+		for(unsigned int j=0; j<islandVec.at(i).size(); j++) {
+			for(unsigned int k=0; k<islandVec.at(i).at(j).size(); k++) {
+				int area = islandVec.at(i).at(j).at(k).area();
+				areaVec.push_back(area);
+				indexVec.push_back(i);
+				indexVec.push_back(j);
+				indexVec.push_back(k);
+				indexVec2d.push_back(indexVec);
+				indexVec.clear();
+			}
+		}
+	}
+	vector<int> origPos;
+	jaysort(areaVec,origPos);
+	int sum = 0;
+	for(int i=areaVec.size()-1; i>=0; i--) {
+		int pos = origPos.at(i);
+		int shape = indexVec2d.at(pos).at(0);
+		int shade = indexVec2d.at(pos).at(1);
+		int idx = indexVec2d.at(pos).at(2);
+		int shade_shift = min(shade-1,0);
+		newIslandVec.at(shape).at(shade_shift).push_back(islandVec.at(shape).at(shade).at(idx));
+		newIslandVec.at(shape).at(shade).erase(newIslandVec.at(shape).at(shade).begin()+idx);
+
+		sum += areaVec.at(i);
+		if(sum>thresh) break;
+
+		printf("shape%d_s%d_%d: %d\n",indexVec2d.at(pos).at(0),indexVec2d.at(pos).at(1),indexVec2d.at(pos).at(2),areaVec.at(i));
+	}
+	return false;
+}
--- WebDerm/src/ShadeShape/ShadeMatch/shadematch.h
+++ WebDerm/src/ShadeShape/ShadeMatch/shadematch.h
@@ -8,8 +8,18 @@
 #ifndef SHADEMATCH_H_
 #define SHADEMATCH_H_
 
-class ShadeMatch {
+#include "/home/jason/git/WebDerm/WebDerm/headers/global.h"
+#include "../shadeshape.h"
+#include "../islands.h"
 
+class ShadeShape;
+class Islands;
+class ShadeMatch {
+private:
+	const int SHIFT_RIGHT = 1;
+	const int SHIFT_LEFT = -1;
+public:
+	bool shade_translation(vector<vector<vector<Islands> > > &islandVec, float totalArea);
 };
 
 #endif /* SHADEMATCH_H_ */
