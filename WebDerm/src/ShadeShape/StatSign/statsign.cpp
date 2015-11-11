06e2c2f01ebd412a3df8f8a4f364723aa0ba2800
Jason <yanjia23@msn.com>
2015-11-11 17:07:48 -0500

    11/11/15 - changed 45.0->1.5

--- WebDerm/src/ShadeShape/StatSign/statsign.cpp
+++ WebDerm/src/ShadeShape/StatSign/statsign.cpp
@@ -103,9 +103,11 @@ float StatSign::proportion(vector<int> statSignVec1, vector<int> statSignVec2) {
 //! adjust the score using inverse tan-hyperbolic function
 float StatSign::adjustValue(float value) {
 	float pMark = 0.65; //> percentage mark where values gets pushed up or down
-	float result = value + (45.0*atanh(value - pMark)) * value;
-	result = min(result,0.9f);
-	result = max(result,0.0f);
+	float result = value + (1.5*atanh(value - pMark)) * value;
+	float upperLimit = 0.9;
+	float lowerLimit = 0.0;
+	result = min(result,upperLimit);
+	result = max(result,lowerLimit);
 	return result;
 }
 
